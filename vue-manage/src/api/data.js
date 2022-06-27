629c7c3c99fa99c192f053eb56b9d1212057059e
xmxZzzz <1019566174@qq.com>
2022-06-28 02:30:26 +0800

    添加用户管理模块

--- vue-manage/src/api/data.js
+++ vue-manage/src/api/data.js
@@ -13,3 +13,26 @@ export const getData = () => {
         url: '/home/getData'
     })
 }
+export const createUser = (param) => {
+    return axios.request({
+        url: '/user/add',
+        method: 'post',
+        data: param
+    })
+}
+
+export const updateUser = (param) => {
+    return axios.request({
+        url: '/user/edit',
+        method: 'post',
+        data: param
+    })
+}
+
+export const getUser = (params) => {
+    return axios.request({
+        url: '/user/getUser',
+        method: 'get',
+        params
+    })
+}
---
+++ vue-manage/src/components/CommomForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-form ref="form" label-width="100px" :model="form" :inline="inline">
+    <el-form-item
+      v-for="item in formLabel"
+      :key="item.label"
+      :label="item.label"
+    >
+      <el-input
+        v-if="item.type === 'input'"
+        v-model="form[item.model]"
+        :placeholder="'请输入' + item.label"
+      ></el-input>
+      <el-switch
+        v-if="item.type === 'switch'"
+        v-model="form[item.model]"
+      ></el-switch>
+      <el-date-picker
+        v-if="item.type === 'date'"
+        type="date"
+        v-model="form[item.model]"
+        value-format="yyyy-MM-dd"
+        placeholder="选择日期"
+      ></el-date-picker>
+      <el-select
+        v-if="item.type === 'select'"
+        v-model="form[item.model]"
+        placeholder="请选择"
+      >
+        <el-option
+          v-for="ele in item.opts"
+          :key="ele.value"
+          :value="ele.value"
+          :label="ele.label"
+        ></el-option>
+      </el-select>
+    </el-form-item>
+    <!-- 自定义item -->
+    <el-form-item><slot></slot></el-form-item>
+  </el-form>
+</template>
+<script>
+export default {
+  name: "CommonForm",
+  props: {
+    formLabel: Array,
+    form: Object,
+    inline: Boolean,
+  },
+};
+</script>
